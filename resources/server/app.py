0218d1718633729a66104fb5a61ca95655db8a7d
pigsty <pigsty@users.noreply.github.com>
2016-07-15 17:39:11 +0100

    add gmail support

--- resources/server/app.py
+++ resources/server/app.py
@@ -13,34 +13,43 @@ app.config.from_pyfile("/config/img_mailer.cfg")
 def info():
   return "To send mail use /mailsend"
 
-
 @app.route("/mailsend")
 def mailsend():
   cherrypy.log("Application triggered")
-  msg = MIMEMultipart()
-  request = urllib2.Request(app.config[IMG_URL])
-  auth = base64.encodestring('%s:%s' % (app.config[USERNAME], app.config[PASSWORD])).replace('\n', '')
+  cherrypy.log("IMG_URL=" + app.config["IMG_URL"])
+  cherrypy.log("USERNAME=" + app.config["USERNAME"])
+  request = urllib2.Request(app.config["IMG_URL"])
+  auth = base64.encodestring('%s:%s' % (app.config["USERNAME"], app.config["PASSWORD"])).replace('\n', '')
   request.add_header("Authorization", "Basic %s" % auth)
-  cherrypy.log("Pulling image from URL: " + app.config[IMG_URL])
+  cherrypy.log("Pulling image from URL: " + app.config["IMG_URL"])
   try:
-    image = MIMEImage(urllib2.urlopen(link).read())
+    image = MIMEImage(urllib2.urlopen(request).read())
   except: 
     cherrypy.log("Failed to load image", traceback=True)
 
   text = MIMEText("")
 
+  msg = MIMEMultipart()
   msg.attach(text)
   msg.attach(image)
+  msg["From"] = app.config["EMAIL_FROM"]
+  msg["To"] = app.config["EMAIL_TO"]
+  msg["Subject"] = "Image Attached"
 
-  mailer = smtplib.SMTP()
-  mailer.connect()
-  cherrypy.log("Sending mail to " + app.config[EMAIL_FROM])
+  cherrypy.log("Connecting to SMTP server as " + app.config["EMAIL_USERNAME"])
+  try: 
+    mailer = smtplib.SMTP_SSL("smtp.gmail.com")
+    mailer.login(app.config["EMAIL_USERNAME"], app.config["EMAIL_PASSWORD"])
+  except:
+    cherrypy.log("Failed to login to SMTP server")
+
+  cherrypy.log("Sending mail to " + app.config["EMAIL_TO"])
   try:
-    mailer.sendmail(app.config[EMAIL_FROM], app.config[EMAIL_TO], msg.as_string())
+    mailer.sendmail(app.config["EMAIL_FROM"], app.config["EMAIL_TO"], msg.as_string())
   except:
     cherrypy.log("Failed to send mail", traceback=True)
 
-  mailer.close()
+  mailer.quit()
 
   return "Ok"
 
